e3571129dee9d583c5eb4f876be66c0cbed5d662
danielrobb <danielrobb@gmail.com>
2019-03-27 14:00:14 -0700

    Before trying to speed up with numba

--- analysis/internal_wave.py
+++ analysis/internal_wave.py
@@ -15,7 +15,7 @@ def calc_beta(gp, h1, h2):
     return calc_c0(gp, h1, h2) * h1 * h2 / 6.
 
 def kdv(t, u, params):
-    """Differential equations for the KdV equation, discretized in x."""
+    """The KdV equation discretized in x."""
     p = params
     k, L, c0, alpha, beta, diss_coeff = (p['k'], p['L'], p['c0'], p['alpha'], p['beta'], p['diss_coeff'])
     uhat = np.fft.fft(u)
@@ -32,8 +32,32 @@ def rk4(f, t, y, dt, c):
     k4 = f(t + dt, y + dt * k3, c)
     return y + dt/6. * (k1 + 2*k2 + 2*k3 + k4)
 
+def solve_kdv(y0, dt, t_span, t_eval, params):
+    t0, tf = t_span
+    t, y = (t0, y0)
+    nt, nx = (len(t_eval), len(y0))
+    sol = np.zeros((nt, nx))
+    id_out = idt = 0
+    while t < tf:
+        y = rk4(kdv, t, y, dt, params)
+        if np.isclose(t, t_eval[id_out], atol=dt/2):
+            print_diagnostics(idt, id_out, dt, t, y)
+            sol[id_out, :] = y
+            id_out += 1
+        t += dt
+        idt += 1
+    sol_wrapped = wrap_solution(sol)
+    return sol_wrapped
+
+def wrap_solution(sol):
+    return sol[:, :int(nx/2)] + np.flip(sol[:, int(nx/2):], axis=1)
+
+def print_diagnostics(idt, id_out, dt, t, u):
+    message = f'iteration: {idt}, output: {id_out}, dt: {dt:.5f}, time: {t:.2f}, min_u: {np.min(u):.5f}, max_u: {np.max(u):.5f}'
+    print(message)    
+
 if __name__ == "__main__":
-    # Set the size of the domain, and create the discretized grid.
+    # Constants
     L = 12.
     H = 0.29
     h2 = 0.058
@@ -46,15 +70,17 @@ if __name__ == "__main__":
     alpha = calc_alpha(gp, h1, h2)
     beta = calc_beta(gp, h1, h2)
     diss_coeff = 0.5 * np.sqrt(0.5 * nu * c0) * (h1 + h2) / (h1 * h2)
-
-    nx = 256
+    print(f'c0: {c0}, alpha: {alpha}, beta: {beta}, diss_coeff: {diss_coeff}')
+    # Grid
+    nx = 512
+    dx = L / nx
     x = np.linspace(0.,L, nx+1)
     x = x[:nx]  
     kx1 = np.linspace(0, nx/2-1, nx/2)
     kx2 = np.linspace(1, nx/2, nx/2)
     kx2 = -1*kx2[::-1]
     k = (2.*np.pi/L)*np.concatenate((kx1, kx2))
-
+    # Initial conditions
     u0 = np.empty_like(x)
     idx = np.arange(0, int(nx/2))
     eta0 = np.tan(theta) * (L/4)
@@ -63,24 +89,17 @@ if __name__ == "__main__":
     u0[idx] = m * (x[idx] - x0) 
     idx = np.arange(int(nx/2), nx) 
     u0[idx] = -m * (x[idx] - 3*x0)
-
-    t0, tf = (0., 300.)
-    nout = 300.
-    t_eval = np.linspace(t0, tf, nout)
-    print("Computing the solution.")
     params = {'k': k, 'L': L, 'c0': c0, 'alpha': alpha, 'beta': beta, 'diss_coeff': diss_coeff}
-    dt = 0.01 
-    t = np.arange(t0, tf, dt)
-    nt = len(t)
-    y = np.zeros((nt, nx))
-    y[0, :] = 0.5 * u0
-    for i in range(nt-1):
-        y[i+1, :] = rk4(kdv, t[i], y[i, :], dt, params)
-    f = y[:, :int(nx/2)]
-    g = np.flip(y[:, int(nx/2):], axis=1)
-    fg = f + g
+    # Time stepping
+    t_span = (0., 300.)
+    (t0, tf) = t_span
+    nout = 300.
+    t_eval = np.linspace(t0, tf, nout+1)    
+    CFL = 0.3
+    dt = (dx)**3 / (c0) ** 3 * CFL
+    y0 = 0.5 * u0
+    sol = solve_kdv(y0, dt, t_span, t_eval, params)
     fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(t, fg[:, int(nx/4)])
+    ax.plot(t_eval, sol[:, int(nx/4)])
     plt.show()
-    #sol = solve_ivp(kdv, t_span=(t0, tf), y0=0.5*u0, method='RK45', t_eval=t_eval, rtol=1e-10, atol=1e-10)
 
