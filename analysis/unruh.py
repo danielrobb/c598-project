623b3104ec20ac10f91cb963c840ba4eae04e636
danielrobb <danielrobb@gmail.com>
2019-04-03 23:42:30 -0700

    unruh working

--- analysis/unruh.py
+++ analysis/unruh.py
@@ -18,13 +18,7 @@ class KdVModel(object):
                  x_span=None,
                  t_span=None,
                  t_out=None,
-                 dt=None,
-                 ul=None,
-                 ur=None,
-                 x0=None,
-                 sigma=None,
-                 npeaks=None,
-                 cfl=1.):
+                 dt=None):
         self.nx = nx
         self.xmin = x_span[0]
         self.xmax = x_span[1]
@@ -34,16 +28,10 @@ class KdVModel(object):
         self.dt = dt
         self.id_out = 0
         self.nout = len(self.t_out)
-        self.cfl = cfl
         self.L = self.xmax - self.xmin
         self.dx = self.L / nx
-        self.x = np.arange(-self.L/2, self.L/2, self.dx)
+        self.x = np.arange(0, self.L, self.dx)
         # Coefficients
-        self.ul = ul
-        self.ur = ur
-        self.x0 = x0
-        self.sigma = sigma
-        self.npeaks = npeaks
         # Time
         self.nt = int(np.ceil(self.tmax/self.dt))
         self.idt = 0
@@ -72,6 +60,7 @@ class KdVModel(object):
                 self._stepforward()
                 self.idt += 1
                 self.t += self.dt
+                #print(self.t)
         except:
             print('foo')
 
@@ -87,6 +76,10 @@ class KdVModel(object):
         self._build_coeff_c()
         self._build_matrix_C()
         self._update_mom()
+        #self.phi[self.idt, -2] = self.phi[self.idt, 0]
+        #self.phi[self.idt, -1] = self.phi[self.idt, 1]
+        #self.mom[self.idt, -2] = self.mom[self.idt, 0]
+        #self.mom[self.idt, -1] = self.mom[self.idt, 1]
 
     def _update_phi(self):
         """Update velocity potential phi."""
@@ -152,11 +145,14 @@ class KdVModel(object):
 
     def _init_variables(self):
         """Initialize variables."""
-        #dtype = np.dtype(np.complex128)
-        dtype = np.dtype(np.float64)
+        dtype = np.dtype(np.complex128)
+        #dtype = np.dtype(np.float64)
         shape = (self.nt, self.nx)
         # Velocity potential
         self.phi = np.zeros(shape, dtype)
+        self.phi0 = np.zeros(self.nx, dtype)
+        self.mom = np.zeros(shape, dtype)
+        self.mom0 = np.zeros(self.nx, dtype)
         self.B = np.zeros((self.nx, self.nx), dtype)
         # Momentum
         self.mom  = np.zeros(shape, dtype)
@@ -181,32 +177,31 @@ class KdVModel(object):
         #self.mom[1, :] = self.mom1
 
     def _calc_v(self):
-         #self.v = (ur + ul)/2. + (ur - ul)/2. * np.tanh(self.amp * self.x)
-        self.v = (self.ur-self.ul)*np.exp(-0.5*(self.x/(self.sigma/5))**2) + ul
+        x = self.x
+        self.v =  0.2 * np.tanh(24*np.cos(2*np.pi*(x-0.25))+23.) - 1.
      
     def _calc_phi0(self):
         x = self.x
-        x0 = self.x0
-        sigma = self.sigma
-        n = self.npeaks
-        self.phi0 = np.cos(n*np.pi*x/sigma)*np.exp(-np.pi*((x-x0)/sigma)**2)
+        x0 = 0.3
+        k0 = 50
+        sigma = 0.025
+        amp = 0.1
+        self.phi0 = amp * np.exp(-0.5*(x-x0)**2/sigma**2 + k0*(x+x0)*1j)
 
     def _calc_mom0(self):
         x = self.x
-        x0 = self.x0
         phi_hat = np.fft.fft(self.phi0)
         f2 = self.disp2
         f = f2 ** 0.5 
-        mom0 = np.real(-1j * np.fft.ifft(f * phi_hat))
-        self.mom0 = np.zeros_like(mom0)
+        mom0 = -1j * np.fft.ifft(f * phi_hat)
         dispersive_term = np.real(np.fft.ifft(f2 * phi_hat)) 
         for i in range(self.nx):
             (i_prev, i_next) = self._get_prev_next(i)
             self.mom0[i] = mom0[i] - self.a*(self.v[i_next]*mom0[i_next] - self.v[i_prev]*mom0[i_prev]) - self.dt / 2. * dispersive_term[i]
 
     def _calc_disp2(self):
-        #self.disp2 = self.k**2*(1. - 0. * self.k**2)
-        self.disp2 = self.k**2 / np.abs(self.v)
+        k0 = 512.
+        self.disp2 = (k0 * np.tanh(self.k / k0))**2
 
     def to_csv(self, filename='out.csv'):
         """Save model results to a csv."""
@@ -214,7 +209,7 @@ class KdVModel(object):
         t = np.tile(self.t_out, self.nx)
         x = np.repeat(self.x, (len(self.t_out)))
         print(phi.shape, t.shape, x.shape)
-        data = {'t': t, 'x': x, 'phi': phi}
+        data = {'t': t, 'x': x, 'phi': np.real(phi)}
         df = pd.DataFrame(data)
         df.to_csv(filename, index=False, float_format='%.5f')
 
@@ -222,10 +217,10 @@ class KdVModel(object):
         idt = self.idt
         id_out = self.id_out
         dt = self.dt
-        umin = np.min(self.phi)
-        umax = np.max(self.phi)
-        mmin = np.min(self.mom)
-        mmax = np.max(self.mom)
+        umin = np.real(np.min(self.phi))
+        umax = np.real(np.max(self.phi))
+        mmin = np.real(np.min(self.mom))
+        mmax = np.real(np.max(self.mom))
         t = self.t
         message = f'iteration: {idt}, output: {id_out}, dt: {dt:.5f},' \
                   + f'time: {t:.2f}, min_u: {umin:.5f}, max_u: {umax:.5f}, min_m: {mmin:.5f}, max_m: {mmax:.5f}'
@@ -233,25 +228,20 @@ class KdVModel(object):
 
     def _plot_check(self):
         plt.plot(self.x, self.phi[0, :])
-        plt.plot(self.x, self.mom[0, :])
+        #plt.plot(self.x, self.mom[0, :])
         plt.plot(self.x, self.v)
         plt.show()
 
 if __name__ == "__main__":
-    # Constants
-    ul = -0.8
-    ur = -1.2
-    x0 = -200.
-    sigma = 200.
-    npeaks = 3.
     # Grid
-    xmin, xmax = (-1000., 1000.)
-    tmin, tmax = (0., 200.)
+    xmin, xmax = (0., 1.)
     nx = 1024
-    nout = 20
+    dx = 1/nx
+    nout = 200
+    k0 = 512
+    dt = dx 
+    tmin, tmax = (0., 4.5)
     t_out = np.linspace(tmin, tmax, nout)
-    dt = 0.25
-    m = KdVModel(nx=nx, x_span=(xmin, xmax), t_span=(tmin, tmax), t_out=t_out,
-                 ul=ul, ur=ur, x0=x0, sigma=sigma, npeaks=npeaks, cfl=1., dt=dt)
+    m = KdVModel(nx=nx, x_span=(xmin, xmax), t_span=(tmin, tmax), t_out=t_out,dt=dt)
     m.run()
-    m.to_csv('../data/robertson.csv')
+    m.to_csv('../data/unruh.csv')
